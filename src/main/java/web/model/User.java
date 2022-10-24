88ab1f2a8e261a84d5eabfe996090967b9688e81
Макс Апаков <maksapakov@gmail.com>
2022-10-24 15:38:25 +0300

    ver.0.01

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,79 @@
+package web.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    public User() {
+    }
+
+    public User(String name, String lastName, byte age, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "Age")
+    private byte age;
+
+    @Column(name = "Email")
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User [id = %d; name = %s; lastName = %s; age = %s; email = %]",
+                               id, name, lastName, age, email);
+    }
+}
---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+
+    @Override
+    public void edit(User user) {
+
+    }
+
+    @Override
+    public User getById(long id) {
+        return null;
+    }
+}

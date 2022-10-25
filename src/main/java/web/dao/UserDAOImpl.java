af03785ee1a8d48470dacb602975f616ed4b7704
Макс Апаков <maksapakov@gmail.com>
2022-10-25 11:20:43 +0300

    ver.0.1

---
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -0,0 +1,48 @@
+package web.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Autowired
+    public UserDAOImpl() {
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("from User", User.class).getResultList();
+    }
+
+    @Override
+    public void save(User user) {
+        entityManager.persist(user);
+        entityManager.flush();
+    }
+
+    @Override
+    public void edit(User user) {
+        entityManager.merge(user);
+        entityManager.flush();
+    }
+
+    @Override
+    public User getById(long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void delete(User user) {
+        entityManager.remove(user);
+        entityManager.flush();
+    }
+}
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,35 +1,45 @@
 package web.service;
 
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.UserDAO;
 import web.model.User;
 
 import java.util.List;
-
+@Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
+    private UserDAO userDAO;
 
+    @Autowired
+    public void setUser (UserDAO userDAO){
+        this.userDAO = userDAO;
+    }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return userDAO.getAllUsers();
     }
 
     @Override
     public void save(User user) {
-
+        userDAO.save(user);
     }
 
     @Override
     public void delete(User user) {
-
+        userDAO.delete(user);
     }
 
     @Override
     public void edit(User user) {
-
+        userDAO.edit(user);
     }
 
     @Override
     public User getById(long id) {
-        return null;
+        return userDAO.getById(id);
     }
 }
